ec4e3c20f7200f1c750b1b12ef25fb02f1590394
ryans439 <ryans439@nycstudents.net>
2025-03-21 11:16:24 -0400

    ddd

---
+++ app/src/components/DropDown.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class = "dropdown">
+        <button @click = "toggleDropdown">
+            {{ selectedSchool || 'Select a School' }}
+        </button>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
