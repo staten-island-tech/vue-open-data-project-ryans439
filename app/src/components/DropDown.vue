18c41ef1751e003eee5230718efede62bcad3bc9
ryans439 <ryans439@nycstudents.net>
2025-03-25 12:04:49 -0400

    hh

--- app/src/components/DropDown.vue
+++ app/src/components/DropDown.vue
@@ -3,12 +3,24 @@
         <button @click = "toggleDropdown">
             {{ selectedSchool || 'Select a School' }}
         </button>
-        
+        <ul>v-if = "isDropdownOpen" class = "dropdown-menu"
+            v-if selectedSchool.value = school
+            <li> v-for = "school in schools" :key = "option" @click = "selectedSchool(school)"
+                {{ school }}    
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+import { ref , onMounted} from 'vue';
+onMounted(async () => { const schools = ref([]);
+let selectedSchool = ref(null);
+const isDropdownOpen = ref(false);  
 
+
+}
+);
 </script>
 
 <style lang="scss" scoped>
