68c935cc8d37e03f44a487508e7b97df95219759
ryans439 <ryans439@nycstudents.net>
2025-03-26 19:19:51 -0400

    d

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
     <h1>NYC School SAT Performance</h1>
-    <SchoolChart />
+    <SchoolChart/>
   </div>
 </template>
 
 <script setup>
 import SchoolChart from '@/components/SchoolChart.vue';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 </script>
-
-=======
-</script>
->>>>>>> parent of 2a91728 (hrtehj65j56j)
-=======
-</script>
->>>>>>> parent of 2a91728 (hrtehj65j56j)
-=======
-</script>
->>>>>>> parent of 2a91728 (hrtehj65j56j)
+  
