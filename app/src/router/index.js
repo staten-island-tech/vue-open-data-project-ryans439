44e064ae441540deda27e988cd909e62105554f2
ryans439 <ryans439@nycstudents.net>
2025-03-26 18:48:24 -0400

    d

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,14 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CardsView from '../views/CardsView.vue'
+=======
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+>>>>>>> parent of 2a91728 (hrtehj65j56j)
 =======
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -9,8 +9,12 @@
 import SchoolChart from '@/components/SchoolChart.vue';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 </script>
 
+=======
+</script>
+>>>>>>> parent of 2a91728 (hrtehj65j56j)
 =======
 </script>
 >>>>>>> parent of 2a91728 (hrtehj65j56j)
